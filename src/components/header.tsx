b4f19bebb06e2af5653b8f9e40eacea95f26002f
tadeu.cesar <tadeu.cesar@worldconnectoficial.com.br>
2024-06-13 17:18:10 -0300

    atualizando codigo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,16 +1,64 @@
-import { Box, Flex, Heading, Spacer, Link } from "@chakra-ui/react";
+import { Box, Flex, Input, InputGroup, InputLeftElement, Spacer, Text, Image } from "@chakra-ui/react";
+import { FaFacebook, FaInstagram, FaSearch, FaWhatsapp } from "react-icons/fa";
 
 const Header = () => {
   return (
-    <Box bg="pink.500" p={4} color="white">
-      <Flex maxW="1200px" mx="auto" align="center">
-        <Heading size="md">Doces e Ideias</Heading>
+    <Box p={4} color="white" height="200px">
+      <Flex maxW="1200px" mx="auto" alignItems="center" height="100%">
+      <Image
+          src="../../public/kisspng-logo-party-service-event-management-birthday-multi-layer-birthday-cake-5b3e31acb3fbc1.2897779015308026047372-removebg-preview.png"
+          alt="Logo Doces e Ideias"
+          boxSize="240px"marginTop={8}
+        />
         <Spacer />
-        <Flex gap={6} >
-          <Link href="/">Inicio </Link>
-          <Link href="/temas">temas </Link>
-          <Link href="/cestas">cestas </Link>
-          <Link href="/doces">doces </Link>
+        
+        <InputGroup width="500px" mx="auto" borderRadius={50}>
+          <InputLeftElement
+            pointerEvents="none"
+            children={<FaSearch color="gray" />}
+          />
+          <Input
+            placeholder="O que você procura..."
+            borderRadius={50}
+            pl="40px" 
+          />
+        </InputGroup>
+        
+        <Spacer />
+        <Flex direction="column" align="center" color="black">
+          <Text mb={2}>Acesse nossas redes sociais</Text>
+          <Flex>
+            <Box 
+              as="a" 
+              href="https://www.instagram.com" 
+              target="_blank" 
+              mx={2} 
+              _hover={{ color: "pink.500", transform: "scale(1.2)" }} 
+              transition="transform 0.2s" 
+            >
+              <FaInstagram size="24px" color="black"/>
+            </Box>
+            <Box 
+              as="a" 
+              href="https://www.whatsapp.com" 
+              target="_blank" 
+              mx={2} 
+              _hover={{ color: "green.500", transform: "scale(1.2)" }} 
+              transition="transform 0.2s" 
+            >
+              <FaWhatsapp size="24px" color="black"/>
+            </Box>
+            <Box 
+              as="a" 
+              href="https://www.facebook.com" 
+              target="_blank" 
+              mx={2} 
+              _hover={{ color: "blue.500", transform: "scale(1.2)" }} 
+              transition="transform 0.2s" 
+            >
+              <FaFacebook size="24px" color="black"/>
+            </Box>
+          </Flex>
         </Flex>
       </Flex>
     </Box>
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,19 +1,186 @@
-import { Box, Heading, Text, Image, Flex } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Button, Popover, PopoverTrigger, PopoverContent, PopoverArrow, PopoverBody, PopoverCloseButton, List, ListItem } from "@chakra-ui/react";
+import { useState } from "react";
 import Header from "../components/header";
 import Footer from "../components/footer";
 
 const Home = () => {
+  const [selectedOption, setSelectedOption] = useState(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleOpen = (text) => {
+    setSelectedOption(text);
+    setIsOpen(true);
+  };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <Box>
       <Header />
-      <Flex direction= 'column' minHeight='100vh'>
-      <Box flex="1" mr={{ base: 0, md: 4 }} mb={{ base: 4, md: 0 }}>
-          <Heading size="lg" mb={4}>Bolos</Heading>
-          <Text>Deliciosos bolos feitos sob medida para qualquer ocasião. Aproveite nossos sabores exclusivos e surpreenda seus convidados!</Text>
-        </Box>
-        <Box flex="1">
-          <Image src="../../public/images-removebg-preview.png" alt="Bolo 1" borderRadius="md" />
-        </Box>
+      <Flex direction="row" minHeight="100vh" marginTop={10} width="full">
+        <Flex
+          width="full"
+          height="60px"
+          bgColor="gray.600"
+          alignItems="center"
+          justifyContent="space-between"
+          padding={6}
+          color="white"
+          fontSize="large"
+        >
+          <Popover
+            placement="bottom"
+            isOpen={isOpen && selectedOption === "Festas"}
+            onClose={handleClose}
+          >
+            <PopoverTrigger>
+              <Button
+                variant="link"
+                color="white"
+                _hover={{ color: "blue.300" }}
+                onClick={() => handleOpen("Festas")}
+              >
+                Festas
+              </Button>
+            </PopoverTrigger>
+            {selectedOption === "Festas" && (
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverBody>
+                  <List color={'#000'}>
+                    <ListItem>Opção 1 para Festas</ListItem>
+                    <ListItem>Opção 2 para Festas</ListItem>
+                    <ListItem>Opção 3 para Festas</ListItem>
+                  </List>
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </Popover>
+          <Spacer />
+          <Popover
+            placement="bottom"
+            isOpen={isOpen && selectedOption === "salgados"}
+            onClose={handleClose}
+          >
+            <PopoverTrigger>
+              <Button
+                variant="link"
+                color="white"
+                _hover={{ color: "blue.300" }}
+                onClick={() => handleOpen("salgados")}
+              >
+                Salgados
+              </Button>
+            </PopoverTrigger>
+            {selectedOption === "salgados" && (
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverBody>
+                <List color={'#000'}>
+                    <ListItem>Opção 1 para Salgados</ListItem>
+                    <ListItem>Opção 2 para Salgados</ListItem>
+                    <ListItem>Opção 3 para Salgados</ListItem>
+                  </List>
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </Popover>
+          <Spacer />
+          <Popover
+            placement="bottom"
+            isOpen={isOpen && selectedOption === "Casamentos"}
+            onClose={handleClose}
+          >
+            <PopoverTrigger>
+              <Button
+                variant="link"
+                color="white"
+                _hover={{ color: "blue.300" }}
+                onClick={() => handleOpen("Casamentos")}
+              >
+                Casamentos
+              </Button>
+            </PopoverTrigger>
+            {selectedOption === "Casamentos" && (
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverBody>
+                  <List color={'#000'}>
+                    <ListItem>Opção 1 para Casamentos</ListItem>
+                    <ListItem>Opção 2 para Casamentos</ListItem>
+                    <ListItem>Opção 3 para Casamentos</ListItem>
+                  </List>
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </Popover>
+          <Spacer />
+          <Popover
+            placement="bottom"
+            isOpen={isOpen && selectedOption === "Bolos"}
+            onClose={handleClose}
+          >
+            <PopoverTrigger>
+              <Button
+                variant="link"
+                color="white"
+                _hover={{ color: "blue.300" }}
+                onClick={() => handleOpen("Bolos")}
+              >
+                Bolos
+              </Button>
+            </PopoverTrigger>
+            {selectedOption === "Bolos" && (
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverBody>
+                  <List color={'#000'}>
+                    <ListItem>Opção 1 para Bolos</ListItem>
+                    <ListItem>Opção 2 para Bolos</ListItem>
+                    <ListItem>Opção 3 para Bolos</ListItem>
+                  </List>
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </Popover>
+          <Spacer />
+          <Popover
+            placement="bottom"
+            isOpen={isOpen && selectedOption === "Outros"}
+            onClose={handleClose}
+          >
+            <PopoverTrigger>
+              <Button
+                variant="link"
+                color="white"
+                _hover={{ color: "blue.300" }}
+                onClick={() => handleOpen("Outros")}
+              >
+                Outros
+              </Button>
+            </PopoverTrigger>
+            {selectedOption === "Outros" && (
+              <PopoverContent>
+                <PopoverArrow />
+                <PopoverCloseButton />
+                <PopoverBody>
+                  <List color={'#000'}>
+                    <ListItem>Opção 1 para Outros</ListItem>
+                    <ListItem>Opção 2 para Outros</ListItem>
+                    <ListItem>Opção 3 para Outros</ListItem>
+                  </List>
+                </PopoverBody>
+              </PopoverContent>
+            )}
+          </Popover>
+          <Spacer />
+        </Flex>
       </Flex>
       <Footer />
     </Box>
